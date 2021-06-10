d88070a51b9922caf04344b12c39ea9dae7de200
balaji <balajics92@gmail.com>
2021-06-10 21:09:00 +0530

    payment gateway is developed

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,15 +5,23 @@ import CartItem from './CartItem';
 import classes from './Cart.module.css';
 import CartContext from '../../store/cart-context';
 import Checkout from './Checkout';
+import StripeCheckout from 'react-stripe-checkout';
 
 const Cart = (props) => {
+    const cartCtx = useContext(CartContext);
+
   const [isCheckout, setIsCheckout] = useState(false);
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [didSubmit, setDidSubmit] = useState(false);
-  const cartCtx = useContext(CartContext);
+  const [isPaid, setIsPaid] = useState(false);
+  const [userDataInfo, setUserDataInfo] = useState();
+  const [totalAmountValue, setTotalAmountValue] = useState(cartCtx.totalAmount.toFixed(2));
+
 
   const totalAmount = `₹${cartCtx.totalAmount.toFixed(2)}`;
   const backendTotalAmount = `${cartCtx.totalAmount.toFixed(2)}`;
+  console.log("backendTotalAmount",backendTotalAmount);
+  const paymentAmount = parseInt(`${cartCtx.totalAmount.toFixed(2)}`)
   const hasItems = cartCtx.items.length > 0;
 
   const cartItemRemoveHandler = (id) => {
@@ -52,9 +60,11 @@ const Cart = (props) => {
       items: cartCtx.items,
       totalAmount: backendTotalAmount,
     }),);
+    setUserDataInfo(userData)
+    setTotalAmountValue(cartCtx.totalAmount.toFixed(2))
     setIsSubmitting(false);
     setDidSubmit(true);
-    cartCtx.clearCart();
+    // cartCtx.clearCart();
   };
 
   const cartItems = (
@@ -101,9 +111,61 @@ const Cart = (props) => {
 
   const isSubmittingModalContent = <p>Sending order data...</p>;
 
+  // const didSubmitModalContent = (
+  //   <React.Fragment>
+  //     <p>Successfully sent the order!</p>
+  //     <div className={classes.actions}>
+  //     <button className={classes.button} onClick={props.onClose}>
+  //       Close
+  //     </button>
+  //   </div>
+  //   </React.Fragment>
+  // );
+  
+  const onToken = async (token) => {
+    console.log(token);
+    const paymentDetails = {
+      customerName: userDataInfo.name,
+      customerMailId: token.email,
+      last4Digit: token.card.last4,
+      totalPayment: totalAmountValue
+    }
+    console.log('pyadetails...', paymentDetails);
+    await fetch('http://localhost:8082/api/v1/payment', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        customerName: userDataInfo.name,
+        customerMailId: token.email,
+        last4Digit: token.card.last4,
+        totalPayment: totalAmountValue
+      }),
+    });
+    setIsPaid(true);
+    cartCtx.clearCart();
+
+  }
+
   const didSubmitModalContent = (
+    
     <React.Fragment>
-      <p>Successfully sent the order!</p>
+      {!isPaid && (<StripeCheckout 
+      token={onToken}
+      name = "FoodIn Payment"
+      currency = "INR"
+      amount = {paymentAmount * 100}
+      stripeKey="pk_test_51J0hT1SIV9vCXPrmVb9ohfz9Egy1270FcesyvW8oB0X865AeNd8Nopm1ue9PPx0fsg9nolUVRV0ctrtwWfJPtSWn00cOzaAllz"
+    />)}
+    
+    </React.Fragment>
+  );
+
+  const didFinalSubmitModalContent = (
+    <React.Fragment>
+      {!isPaid && (<p className={classes.doPayment}>Please Click on Pay With Card to initiate Payment!</p>)}
+      {isPaid && (<p className={classes.afterPayment}>Payment Successful!</p>)}
       <div className={classes.actions}>
       <button className={classes.button} onClick={props.onClose}>
         Close
@@ -117,6 +179,7 @@ const Cart = (props) => {
       {!isSubmitting && !didSubmit && cartModalContent}
       {isSubmitting && isSubmittingModalContent}
       {!isSubmitting && didSubmit && didSubmitModalContent}
+      {!isSubmitting && didSubmit && didFinalSubmitModalContent}
     </Modal>
   );
 };
